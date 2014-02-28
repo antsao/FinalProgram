5c2ad55d4cb028e5a0039161ae93e57ee2a1bba0
antsao <antsao@calpoly.edu>
2014-02-27 22:03:43 -0800

    Updated cuda file and added header file

---
+++ ParticleUpdate.cu
@@ -0,0 +1,41 @@
+#include "ParticleUpdate.h"
+
+Particle *updateParticles(Particle *particles, int size) {
+   dim3 dimBlock(1024);
+   dim3 dimGrid(1024);
+
+   my_vec3 *localExtForce;
+   localExtForce->x = 0;
+   localExtForce->y = -3;
+   localExtForce->z = 0;
+
+   Particle *d_particles;
+   cudaMalloc(&d_particles, sizeof(Particle) * size);
+   cudaMemcpy(d_particles, particles, sizeof(Particle) * size, cudaMemcpyHostToDevice);
+
+   my_vec3 *d_localExtForce;
+   cudaMalloc(&d_localExtForce, sizeof(my_vec3));
+   cudaMemcpy(d_localExtForce, localExtForce, sizeof(my_vec3), cudaMemcpyHostToDevice);
+
+   updateParticleKernel<<dimBlock, dimGrid>>(particles, localExtForce);
+
+   cudaMemcpy(particles, d_particles, sizeof(Particle) * size, cudaMemcpyDeviceToHost);
+
+   cudaFree(d_particles);
+   cudaFree(d_localExtForce);
+   return particles;
+}
+
+__global__ void updateParticleKernel(Particle *particles, my_vec3 extForce) {
+   int pInd = BlockIdx.x * threadIdx.x;
+   float deltaTime = 0.01;
+   Particle part = particles[pInd];
+
+   part.velocity.x = part.velocity.x + exterForce.x*deltaTime;
+   part.velocity.y = part.velocity.y + exterForce.y*deltaTime;
+   part.velocity.z = part.velocity.z + exterForce.z*deltaTime;
+
+   part.position.x = part.position.x + part.velocity.x * deltaTime;
+   part.position.y = part.position.y + part.velocity.y * deltaTime;
+   part.position.z = part.position.z + part.velocity.z * deltaTime;
+}
---
+++ ParticleUpdate.h
@@ -0,0 +1,10 @@
+#ifndef PARTICLE_UPDATE_H
+#define PARTICLE_UPDATE_H
+
+#include <cuda.h>
+#include <cuda_runtime_api.h>
+
+Particle *updateParticles(Particle *particles, int size);
+__global__ void updateParticleKernel(Particle *particles, my_vec3 extForce);
+
+#endif
